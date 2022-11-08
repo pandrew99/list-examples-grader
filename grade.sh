673d89334940bd67b96145607d0900539bd2bde9
Andrew Pan <ajpan@ucsd.edu>
2022-11-08 14:00:26 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -2,6 +2,26 @@
 
 set -e
 
+# step 1
 rm -rf student-submission
 git clone $1 student-submission
 
+# step 2
+if find type -type f -name "ListExamples.java" -eq true
+then echo "ListExamples.java file found!"
+else echo "ListExamples.java file not found. Please check to make sure you submitted the right file"
+exit
+
+# step 3
+cp TestListExamples.java student-submission/
+cd student-submission/
+
+# step 4
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/ijunit-4.13.2.jar *.java
+if $? != 0
+then
+echo "Your code did not compile. Check to make sure you don't have any syntax errors."
+fi
+
+# step 5
+java TestListExamples
